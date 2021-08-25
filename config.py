ad8832694b108d61fc8b8b724745e32185ffed9a
franklin <franklinwanami@gmail.com>
2021-08-25 15:53:32 +0300

    updated my html

--- config.py
+++ config.py
@@ -3,16 +3,18 @@ import os
 
 class Config:
     """Main configurations class"""
-    SQLACHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://garlinsk:kenya254@localhost/blog'
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
-    MAIL_SERVER = 'smtp.gmail.com'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/images'
+    MAIL_SERVER = 'smtp@gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
+    
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export MAIL_USERNAME="frank@gmail.com"
 export MAIL_PASSWORD="kenya254"
 export SECRET_KEY="Qwerty"
-python3.9 manage.py
+
+python3.9 manage.py server
